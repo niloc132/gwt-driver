3dfa38b2d330ea7b9d8813f17acc5fc65a8dbfa8
Colin Alworth <niloc132@gmail.com>
2012-08-14 09:51:34 -0500

    Starting to make some testing stuff so that a gwt module can be compiled and tested as part of the build phase

--- src/test/java/com/colinalworth/gwtdriver/models/SimpleWidgetTest.java
+++ src/test/java/com/colinalworth/gwtdriver/models/SimpleWidgetTest.java
@@ -1,5 +1,12 @@
 package com.colinalworth.gwtdriver.models;
 
+import java.io.File;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import com.google.gwt.dev.shell.jetty.JettyLauncher;
+import com.google.gwt.dev.util.log.PrintWriterTreeLogger;
 import com.google.gwt.user.client.ui.RootPanel;
 
 public class SimpleWidgetTest {
@@ -12,5 +19,21 @@ public class SimpleWidgetTest {
 		SmokeTestWidget test = new SmokeTestWidget();
 		
 		
+	}
+	
+	public void testWithDriver() throws Exception {
+		//start up server
+		JettyLauncher server = new JettyLauncher();
+		server.setBindAddress("localhost");
+		server.start(new PrintWriterTreeLogger(), 1234, new File("target/www"));
+		
+		Thread.sleep(10000);
+		//start up browser
+		WebDriver wd = new HtmlUnitDriver(true);
+		wd.get("http://localhost:1234/");
+		
+		GwtWidget widget = new GwtRootPanel(wd);
+		
+//		assert widget.as(clazz)
 	}
 }
